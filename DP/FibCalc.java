f71b34050f1540206757badf129d9231fe122636
adityavhegde <adityavhegde@gmail.com>
2016-04-17 13:49:14 +0530

    Folder added

---
+++ DP/FibCalc.java
@@ -0,0 +1,32 @@
+
+package DP;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Aditya
+ */
+/*
+FIND THE Nth FIBONACCI NUMBER
+*/
+public class FibCalc {
+    public static Hashtable h;
+    
+    public static void main(String[] args) {
+        h=new Hashtable();
+        System.out.println(fib(4)+" "+fib(5));
+        
+    }
+    
+    public static int fib(int n){
+        if (h.containsKey(n)) return (int)h.get(n);
+        if (n<=2) return 1;
+        else {
+            int f=fib(n-1)+fib(n-2);
+            h.put(n, f);
+            return f;
+        }
+    }
+    
+}
