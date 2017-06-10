6a4fd7b2b3fd9d31f00410a7886465dfefcca23b
Aditya Hegde <adityavhegde@gmail.com>
2017-06-10 19:19:17 +0530

    non repetition part added

--- DpAndRecursive/Permutations.java
+++ DpAndRecursive/Permutations.java
@@ -3,7 +3,8 @@ package DpAndRecursive;
 
 /**
  * Problem: generate all permutations of a string
- * Note: A character can be repeated
+ * Proc 1: repeats characters for eg aab
+ * Proc 2 does not repeat character and above permutations won't be generated
  * Extended the logic from NumbersAddToN problem
  * @author Aditya
  */
@@ -24,6 +25,22 @@ public class Permutations {
         for(int i = 0; i<input.length(); i++){
             if(N-1 >= 0){
                 proc(res.concat(Integer.toString(i)),N-1);
+            }
+        } 
+
+    }
+    static void proc(String res, int start, int N){
+        
+        if(N==0){
+            for(int j=0; j<res.length();j++){
+                System.out.print(input.charAt((int)(res.charAt(j)-48)));
+            }
+            System.out.println("");
+        }
+
+        for(int i = start; i<input.length(); i++){
+            if(N-1 >= 0){
+                proc(res.concat(Integer.toString(i)), start + 1, N - 1);
             }
         } 
 
